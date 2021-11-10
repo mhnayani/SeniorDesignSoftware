ec16533002fb7e8aa94acbfebd7bb9ed00d903f3
Zach Jordan <zachjordan2018@gmail.com>
2021-11-09 20:32:20 -0600

    add comment

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* Put weather data and image here */}
+      <ScrollView>
+        {/*Put sensor data box components in here.*/}
+      </ScrollView>
     </View>
   );
 }
