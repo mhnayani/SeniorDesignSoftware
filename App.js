81e210f0119cac1c5309bde17f948a8cb7a09c9d
Zach Jordan <zachjordan2018@gmail.com>
2021-11-10 16:55:08 -0600

    start adding api

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+{/* https://openweathermap.org/current */}
+const weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=richardson,tx&appid=c8994bd806fee3ad5b68daf9cc245e95";
 
 export default function App() {
+
+  const getWeatherData = async () => {
+    const response = await fetch(weatherApiUrl);
+    const jsonData = await response.json();
+    setUserData(jsonData);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button>{/* Put a right-aligned refresh button here? */}</Button>
+      <Text> {/* Put center-aligned word for current weather here (e.g. Clear, Raining, etc..) */} </Text>
+      {/* Put weather image here based on what the weather is. */}
       {/* Put weather data and image here */}
-      <ScrollView>
-        {/*Put sensor data box components in here.*/}
-      </ScrollView>
+      {/* Put sensor data box components in here.*/}
     </View>
   );
 }
@@ -18,8 +29,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#65a7b3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+
+  },
+  weatherText: {
+
+  }
 });
